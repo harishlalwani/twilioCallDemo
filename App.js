af13cdadfb22d6a2b6500f26f90222cd6d025536
harish lalwani <harrie006@gmail.com>
2023-01-27 13:42:04 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,220 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Component, useRef, useState } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {
+  TextInput,
+  Button,
+  PermissionsAndroid,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+
+import {
+  TwilioVideo,
+  TwilioVideoLocalView,
+  TwilioVideoParticipantView
+} from 'react-native-twilio-video-webrtc'
+
+import styleSheet from "./styles";
+
+const styles = StyleSheet.create(styleSheet);
+
+async function fetchData (url) {
+  let response = await fetch(url);
+  return await response.json()  ;
+}
+
+export default App = (props) => {
+  const [isAudioEnabled, setIsAudioEnabled] = useState(true);
+  const [isVideoEnabled, setIsVideoEnabled] = useState(true);
+  const [status, setStatus] = useState('disconnected');
+  const [participants, setParticipants] = useState(new Map());
+  const [videoTracks, setVideoTracks] = useState(new Map());
+  const [token, setToken] = useState('');
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const twilioRef = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // disconnect call after 15 mins
+      twilioRef.current.disconnect();
+    }, 900); // 15 * 60 - 15 mins
+    return () => clearTimeout(timer);
+  }, []);
+
+  const _onConnectButtonPress = async () => {
+    let end = "http://192.168.1.16:3000/generate_token";
+    let url = end+"?identity="+name+"&room="+room;
+    let accessToken = (await fetchData(url, { type: 'text' }))
+    // accessToken = accessToken.accessToken
+    // console.log('accessToken', accessToken, url);
+    twilioRef.current.connect(accessToken);
+    setStatus('connecting');
+  }
+  
+  const _onEndButtonPress = () => {
+    twilioRef.current.disconnect();
+  };
+
+  const _onMuteButtonPress = () => {
+    twilioRef.current
+      .setLocalAudioEnabled(!isAudioEnabled)
+      .then(isEnabled => setIsAudioEnabled(isEnabled));
+  };
+
+  const _onFlipButtonPress = () => {
+    twilioRef.current.flipCamera();
+  };
+
+  const _onRoomDidConnect = ({roomName, error}) => {
+    console.log('onRoomDidConnect: ', roomName);
+
+    setStatus('connected');
+  };
+
+  const _onRoomDidDisconnect = ({ roomName, error }) => {
+    console.log('[Disconnect]ERROR: ', error);
+
+    setStatus('disconnected');
+  };
+
+  const _onRoomDidFailToConnect = error => {
+    console.log('[FailToConnect]ERROR: ', error);
+
+    setStatus('disconnected');
+  };
+
+  const _onParticipantAddedVideoTrack = ({ participant, track }) => {
+    console.log('onParticipantAddedVideoTrack: ', participant, track);
+
+    setVideoTracks(
+      new Map([
+        ...videoTracks,
+        [
+          track.trackSid,
+          { participantSid: participant.sid, videoTrackSid: track.trackSid },
+        ],
+      ]),
+    );
+  };
+
+  const _onParticipantRemovedVideoTrack = ({ participant, track }) => {
+    console.log('onParticipantRemovedVideoTrack: ', participant, track);
+
+    const videoTracksLocal = videoTracks;
+    videoTracksLocal.delete(track.trackSid);
+
+    setVideoTracks(videoTracksLocal);
+  };
+
+  return (
+    <View style={styles.container}>
+      {
+        status === 'disconnected' &&
+        <View>
+          <Text style={styles.welcome}>
+            React Native Twilio Video
+          </Text>
+          <TextInput
+            placeholder="name"
+            style={styles.input}
+            autoCapitalize='none'
+            value={name}
+            onChangeText={(text) => setName(text)}>
+          </TextInput>
+          <TextInput
+            placeholder="room"
+            style={styles.input}
+            autoCapitalize='none'
+            value={room}
+            onChangeText={(text) => setRoom(text)}>
+          </TextInput>
+          <Button
+            title="Connect"
+            style={styles.button}
+            onPress={_onConnectButtonPress}>
+          </Button>
+        </View>
+      }
+
+      {
+        (status === 'connected' || status === 'connecting') &&
+          <View style={styles.callContainer}>
+          {
+            status === 'connected' &&
+            <View style={styles.remoteGrid}>
+              {
+                Array.from(videoTracks, ([trackSid, trackIdentifier]) => {
+                  return (
+                    <TwilioVideoParticipantView
+                      style={styles.remoteVideo}
+                      key={trackSid}
+                      trackIdentifier={trackIdentifier}
+                    />
+                  )
+                })
+              }
+            </View>
+          }
+          <View
+            style={styles.optionsContainer}>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={_onEndButtonPress}>
+              <Text style={{fontSize: 12}}>End</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={_onMuteButtonPress}>
+              <Text style={{fontSize: 12}}>{ isAudioEnabled ? "Mute" : "Unmute" }</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.optionButton}
+              onPress={_onFlipButtonPress}>
+              <Text style={{fontSize: 12}}>Flip</Text>
+            </TouchableOpacity>
+            <TwilioVideoLocalView
+              enabled={true}
+              style={styles.localVideo}
+            />
+          </View>
+        </View>
+      }
+
+      <TwilioVideo
+        ref={ twilioRef }
+        onRoomDidConnect={ _onRoomDidConnect }
+        onRoomDidDisconnect={ _onRoomDidDisconnect }
+        onRoomDidFailToConnect= { _onRoomDidFailToConnect }
+        onParticipantAddedVideoTrack={ _onParticipantAddedVideoTrack }
+        onParticipantRemovedVideoTrack= { _onParticipantRemovedVideoTrack }
+      />
+    </View>
+  );
+}
+
